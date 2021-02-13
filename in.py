4293e9a76563fba9616c0f62580d779e28918b75
nextlast <nextlast@163.com>
2021-02-13 19:24:42 +0800

    Add files via upload

---
+++ in.py
@@ -0,0 +1,94 @@
+﻿# -*- coding: utf-8 -*-
+import arcpy
+import string
+import os
+err="第{}个要素,点{},创建错误"
+msg="导入了{}个要素"
+cur=None
+try:
+    txt=arcpy.GetParameterAsText(0)
+    outfc=arcpy.GetParameterAsText(1)
+    spatial=arcpy.SpatialReference(4528)
+    txtfile=open(txt)
+    arcpy.CreateFeatureclass_management(os.path.dirname(outfc),os.path.basename(outfc),"POLYGON","", "", "",spatial)
+    arcpy.AddField_management(outfc,"pointno","LONG")
+    cur=arcpy.da.InsertCursor(outfc,["SHAPE@"]) 
+    arr=arcpy.Array()              
+    array=arcpy.Array()
+
+    featno=0
+    ringid=-1
+    featid=-1
+
+    for line in txtfile.readlines():
+        values = line.replace("\n", "").replace("\r", "").split(",")
+        if len(values) >= 9 and values[len(values)-1].strip() == "@" and featno== 0:
+            featno+=1
+            ringid=-1
+
+        elif len(values) ==4 and featno>=1:
+            if ringid==-1:
+                ringid=values[1]
+            if ringid!=values[1]:
+                array.add(arr)
+                arr.removeAll()
+            p=arcpy.Point()
+            try:
+                p.X=values[3]
+                p.Y=values[2]
+            except:
+                raise Exception,err
+            arr.add(p)
+            ringid=values[1]
+
+        elif len(values) >= 9 and values[len(values)-1].strip() == "@" and featno>=1:
+            array.add(arr)
+            cur.insertRow([arcpy.Polygon(array)])
+            array.removeAll()
+            arr.removeAll()
+            featno +=1
+            ringid=-1
+
+        elif len(values) == 6:
+            if featid==-1:
+                featid=values[1]
+		featno+=1
+	    if ringid==-1:
+                ringid=values[2]
+            if featid==values[1] and ringid!=values[2]:
+                array.add(arr)
+                arr.removeAll()
+            if featid!=values[1]:
+                array.add(arr)
+                cur.insertRow([arcpy.Polygon(array)])
+                array.removeAll()
+                arr.removeAll() 
+                featno+=1   
+            p=arcpy.Point()
+            try:
+                p.X=values[5]
+                p.Y=values[4]
+            except:
+                raise Exception,err
+            arr.add(p)
+            ringid=values[2]
+            featid=values[1]
+
+    array.add(arr)
+    cur.insertRow([arcpy.Polygon(array)])
+    array.removeAll()
+    arr.removeAll()
+    arcpy.AddMessage(msg.format(featno))
+
+except Exception as e:
+    if str(e)==err:
+        err=err.format(featno,values[0])
+        arcpy.AddError(err)
+    elif str(e)!="":
+        arcpy.AddError(str(e))
+    arcpy.AddError(arcpy.GetMessages(2))
+finally:
+    if cur:
+        del cur
+    if txtfile:
+        txtfile.close()
---
+++ out.py
@@ -0,0 +1,60 @@
+﻿# -*- coding: utf-8 -*-
+import arcpy
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+def u2g(str):
+    return str.decode('utf-8').encode('gbk')
+
+def getlst(row):
+
+    lst=[]
+
+    partno=1
+
+    for part in row[1]:
+        startno=1
+        showno=startno
+        
+        for point in part:
+            if point:
+                lst.append([showno,partno,point.Y,point.X])
+            else:
+                lastof=len(lst)-1
+                lst[lastof][0]=startno
+                startno=showno
+                showno-=1
+                partno+=1
+            showno+=1
+        lastof=len(lst)-1
+        lst[lastof][0]=startno   
+        partno+=1
+    return lst
+
+
+shpfile=arcpy.GetParameterAsText(0)
+txt=arcpy.GetParameterAsText(1)
+fo=open(txt,'w')
+
+a1="[属性描述]\n"
+a2="坐标系=2000国家大地坐标系\n"
+a3="几度分带=3\n"
+a4="投影类型=高斯克吕格\n"
+a5="计量单位=米\n"
+a6="带号=40\n"
+a7="精度=0.0100\n"
+a8="转换参数=,,,,,,\n"
+a9="[地块坐标]\n"
+
+
+fo.write(u2g(a1+a2+a3+a4+a5+a6+a7+a8+a9))
+for row in arcpy.da.SearchCursor(shpfile,["OID@","SHAPE@"]):
+
+    fo.write("{},{},{},{},{},,,,@\n".format(row[1].pointCount,int(row[1].area),row[0],row[0],u2g("面")))
+    lst=getlst(row)
+    for a in lst:
+	fo.write("{},{},{},{}\n".format(a[0],a[1],a[2],a[3]))
+
+fo.flush()
+fo.close()
